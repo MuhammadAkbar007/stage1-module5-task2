20ad8073d3f1a48706decd334288ef28079eb6b5
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-06-13 17:46:18 +0500

    Bismillah 🥇

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,13 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (List<Integer> list) -> list.stream()
+                .map(element -> element / divider)
+                .collect(Collectors.toList());
     }
 }
